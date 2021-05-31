39101dcf2c3c93055ca8ea033aa041864d1d0e3e
liangshen <2217291675@qq.com>
2021-05-31 20:48:31 +0800

    ls commit

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,46 @@
 <template>
   <div id="app">
-    <div class="tabbar">
-		<div>书架</div>
-	</div>
+	<tab-bar>
+		<tab-bar-item path='/read'>
+			<img src="./assets/img/tarbar/阅读.svg" slot="item-icon">
+			<img src="./assets/img/tarbar/阅读-active.svg" slot="item-icon-active">
+			<div slot="item-text">阅读</div>
+		</tab-bar-item>
+		<tab-bar-item path='/bookshelf'>
+			<img src="./assets/img/tarbar/书架.svg" slot="item-icon">
+			<img src="./assets/img/tarbar/书架-active.svg" slot="item-icon-active">
+			<div slot="item-text">书架</div>
+		</tab-bar-item>
+		<tab-bar-item path='/community'>
+			<img src="./assets/img/tarbar/社区.svg" slot="item-icon">
+			<img src="./assets/img/tarbar/社区-active.svg" slot="item-icon-active">
+			<div slot="item-text">社区</div>
+		</tab-bar-item>
+		<tab-bar-item path='/profile'>
+			<img src="./assets/img/tarbar/个人中心.svg" slot="item-icon">
+			<img src="./assets/img/tarbar/个人中心-active.svg" slot="item-icon-active">
+			<div slot="item-text">我的</div>
+		</tab-bar-item>
+	</tab-bar>
 	<router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* const TabBar=()=>import('./components/TabBar.vue');
+const TabBarItem=()=>import('./components/TabBarItem.vue'); */
+import TabBar from './components/TabBar.vue';
+import TabBarItem from './components/TabBarItem.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TabBar,
+	TabBarItem
   }
 }
 </script>
-
+	
 <style>
-
-*{
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	@import url('./assets/css/base.css');
 </style>
---
+++ src/pages/read/read.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h2>阅读</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+</style>
